e85507f7871fd8e03d1c44035726f99439430134
leelynne <leelynne@gmail.com>
2013-05-07 14:12:07 -0700

    Get rid of previous emacs setup for prelude.  Add home directory setup script.

--- .emacs.d/elpa/csv-mode-1.0/csv-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "csv-mode" "1.0" "Major mode for editing comma/char separated values" (quote nil))
---
+++ setup_home.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for file in .aliases .environment .bashrc .bash_profile .tmux.conf
+do
+    ln -s $DIR/$file $HOME
+done
+
+# Install Prelude
+export PRELUDE_URL="https://github.com/leelynne/prelude.git"
+curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
