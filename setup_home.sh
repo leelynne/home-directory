88961e0aec9e485de4bb5829b42062feb7917fd6
leelynne <leelynne@gmail.com>
2014-07-15 11:09:36 -0700

    Add .Xresources

--- setup_home.sh
+++ setup_home.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for file in .aliases .environment .bashrc .bash_profile .tmux.conf .xprofile .rbenv
+for file in .aliases .environment .bashrc .bash_profile .tmux.conf .xprofile .Xresources
 do
     ln -s $DIR/$file $HOME
 done
