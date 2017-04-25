91bc71afbfcfc93520836937d341068b13e1999d
Lee <leelynne@gmail.com>
2017-04-25 08:55:56 -0700

    Update home setup after test

--- setup_home.sh
+++ setup_home.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+mkdir $HOME/.history.d
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for file in .aliases .environment .bashrc .bash_profile .tmux.conf .xprofile .Xresources
 do
-    ln -s $DIR/$file $HOME
+    ln -sf $DIR/$file $HOME
 done
