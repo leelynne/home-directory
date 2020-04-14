173a4136eb74279053434adf7d072938294d74e8
Lee <leelynne+gh@protonmail.com>
2020-04-14 12:33:09 -0700

    Require prelude go module

--- emacs/prelude-modules.el
+++ emacs/prelude-modules.el
@@ -12,7 +12,7 @@
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erc)
 ;; (require 'prelude-erlang)
-;;(require 'prelude-go)
+(require 'prelude-go)
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
 ;; (require 'prelude-latex)
